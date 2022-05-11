bed25263cd3eae30b0b5cf0c133de0ba053b5483
Jose A Ruiz-Arias <94106583+jararias@users.noreply.github.com>
2022-05-11 12:55:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
+name = "mpsplines"
 version = "0.1"
 
+version_file = Path(f'{name}/_version.py')
+with open(version_fname, 'w') as f:
+    f.write(f'__version__ = "{version}"')
+
 setup(
-    name="mpsplines",
+    name=name,
     version=version,
     author="Jose A. Ruiz-Arias",
     author_email="jararias@uma.es",
     url="",
     description="",
+    long_description=read_content("README.md")
+    long_description_content_type='text/markdown',
     packages=find_packages(),
-    classifiers=[],
+    classifiers=[
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python ::3.9",
+        "Development Status :: 3 - Alpha"
+    ],
     python_requires=">=3.6",
     install_requires=['numpy', 'scipy', 'loguru'],
+    extras_require={},
     entry_points={}
 )
+
+version_file.unlink()
